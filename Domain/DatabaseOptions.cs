924ea2943d4603f2c6806388b03852b88afe85b1
CaracuianuMihail <caracuianu.mihail@gmail.com>
2025-11-20 21:33:11 +0200

    Changed conn string db for release

--- Domain/DatabaseOptions.cs
+++ Domain/DatabaseOptions.cs
@@ -12,7 +12,7 @@ namespace Domain
         public const string DatabaseConnectionString = @"Data Source=CARACUIANUMIHAI\MSSQLSERVERPAD;Initial Catalog=PostMaker;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=60;Encrypt=True;Trust Server Certificate=True;Command Timeout=0";
 #endif
 #if RELEASE
-        public const string DatabaseConnectionString = @"";
+        public const string DatabaseConnectionString = @"Server=tcp:padserver.database.windows.net,1433;Initial Catalog=PostMaker;Persist Security Info=False;User ID=pad;Password=UTMti221;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 #endif
     }
 }
